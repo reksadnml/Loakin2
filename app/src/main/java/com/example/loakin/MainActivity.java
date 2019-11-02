1193f9962542e2d5692bde8614a2a90bdd2fd10c
reksadnml <reksa.milenia@gmail.com>
2019-11-02 09:27:37 +0700

    first commit

---
+++ app/src/main/java/com/example/loakin/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.loakin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getSupportActionBar().hide();
+
+        Button buttonMasuk = findViewById(R.id.buttonMasuk);
+
+        buttonMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, bottomNav.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
