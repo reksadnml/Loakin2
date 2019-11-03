9e26e34fe0bf81127cee98ad26855bc09699663e
reksadnml <reksa.milenia@gmail.com>
2019-11-03 08:24:21 +0700

    rapiin button

--- app/src/main/java/com/example/loakin/MainActivity.java
+++ app/src/main/java/com/example/loakin/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, bottomNav.class);
                 startActivity(i);
-                finish();
+
             }
         });
     }
