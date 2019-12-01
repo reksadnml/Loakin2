44ffa2038224f9558a7d329c8b86b0390362ea8b
muklasr <muclasrachmanto@gmail.com>
2019-12-01 17:23:43 +0700

    add intent and image picker

--- app/src/main/java/com/example/loakin/CariDriver.java
+++ app/src/main/java/com/example/loakin/CariDriver.java
@@ -1,19 +1,23 @@
 package com.example.loakin;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Button;
+import android.provider.MediaStore;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import java.io.File;
+import java.io.IOException;
 
-import com.example.loakin.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CariDriver extends AppCompatActivity {
 
     private ImageButton searchButton;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,8 @@ public class CariDriver extends AppCompatActivity {
         setContentView(R.layout.activity_cari_driver);
 
         searchButton = findViewById(R.id.search_btn);
+        image = findViewById(R.id.image);
+        Button btnSearchDriver = findViewById(R.id.btnSearchDriver);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +34,43 @@ public class CariDriver extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        btnSearchDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CariDriver.this, OrderActivity.class);
+                startActivity(i);
+            }
+        });
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
 
         getSupportActionBar().hide();
     }
+
+    private void chooseImage(){
+        Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 2);
+    }
+
+//    @Override
+//    void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == 2 && resultCode == RESULT_OK && data != null && data.data != null){
+//            Uri filePath = data.data;
+//            try {
+//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+//                image.setImageBitmap(bitmap);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//    }
+
 }
