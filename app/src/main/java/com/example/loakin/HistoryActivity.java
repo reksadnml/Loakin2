fb242d8e1214f5ef7810a846f92aba41a25525f5
muklasr <muclasrachmanto@gmail.com>
2019-12-02 08:55:05 +0700

    lupa

--- app/src/main/java/com/example/loakin/HistoryActivity.java
+++ app/src/main/java/com/example/loakin/HistoryActivity.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 
-public class History extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#008577\">" + "History" + "</font>"));
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#008577\">" + "HistoryActivity" + "</font>"));
 
 
     }
