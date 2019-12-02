44ae036ada75e2272283ad0947872990071b9382
muklasr <muclasrachmanto@gmail.com>
2019-12-02 08:55:13 +0700

    lupa

--- app/src/main/java/com/example/loakin/MainActivity.java
+++ app/src/main/java/com/example/loakin/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         daftarDriver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, DaftarDriver.class);
+                Intent i = new Intent(MainActivity.this, DaftarDriverActivity.class);
                 startActivity(i);
 
             }
