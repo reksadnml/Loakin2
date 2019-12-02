78cc3ce713be829268aec23269ddfab01ac27f2a
muklasr <muclasrachmanto@gmail.com>
2019-12-02 08:55:57 +0700

    lupa

--- app/src/main/java/com/example/loakin/FeedbackActivity.java
+++ app/src/main/java/com/example/loakin/FeedbackActivity.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 
-public class Feedback extends AppCompatActivity {
+public class FeedbackActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#008577\">" + "Feedback" + "</font>"));
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#008577\">" + "FeedbackActivity" + "</font>"));
     }
 }
